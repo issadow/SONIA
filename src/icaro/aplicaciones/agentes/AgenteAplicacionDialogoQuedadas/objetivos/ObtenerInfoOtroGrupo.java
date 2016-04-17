e18f56d2622f57e7bd4625831151229ca7ce5d0f
Mariano Hernandez <Mariano Hernandez>
2016-04-17 19:17:47 +0200

    Añadido objetivo

---
+++ src/icaro/aplicaciones/agentes/AgenteAplicacionDialogoQuedadas/objetivos/ObtenerInfoOtroGrupo.java
@@ -0,0 +1,11 @@
+package icaro.aplicaciones.agentes.AgenteAplicacionDialogoQuedadas.objetivos;
+
+import icaro.infraestructura.entidadesBasicas.procesadorCognitivo.Objetivo;
+
+public class ObtenerInfoOtroGrupo extends Objetivo {
+	
+	public ObtenerInfoOtroGrupo() {
+		super.setgoalId("ObtenerInfoOtroGrupo");
+	}
+
+}
