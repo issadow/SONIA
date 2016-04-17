1cac130f4d5c61347d069ed4bb636d30e21db89d
Mariano Hernandez <marianoh@ucm.es>
2016-04-17 19:47:48 +0200

    Añadidos primeros tipos de mensajes para las quedadas

---
+++ src/icaro/aplicaciones/agentes/AgenteAplicacionDialogoQuedadas/tools/tiposNotificionQuedadas.java
@@ -0,0 +1,23 @@
+package icaro.aplicaciones.agentes.AgenteAplicacionDialogoQuedadas.tools;
+
+
+/**
+ * Tipos de mensajes de las notificaciones para las quedadas.
+ * 
+ * @author mariano
+ *
+ */
+public class tiposNotificionQuedadas {
+	
+	public static String inicioPeticion	 = "InicioPeticion";
+	public static String numeroOtroGrupo = "numeroOtroGrupo";
+	public static String edadOtroGrupo	 = "edadOtroGrupo";
+	public static String sexoOtroGrupo   = "sexoOtroGrupo";
+	public static String que			 = "que";
+	public static String donde			 = "donde";
+	public static String cuando			 = "cuando";
+	public static String si				 = "si";
+	public static String no				 = "no";
+	public static String sesion			 = "session";
+
+}
